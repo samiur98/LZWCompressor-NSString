ace1d6f69c402bc260d4cd67641a71411bf084ae
samiur98 <sam@Shahs-MacBook-Pro.local>
2020-04-14 13:48:23 -0700

    Init Commit

---
+++ LZWCompressor-NSString/LZWCompressor-NSString/LZWCompressor_NSString.h
@@ -0,0 +1,23 @@
+//
+//  LZWCompressor_NSString.h
+//  LZWCompressor-NSString
+//
+//  Created by Shah Samiur Rahman on 4/14/20.
+//  Copyright © 2020 Shah Samiur Rahman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LZWCompressor_NSString.
+FOUNDATION_EXPORT double LZWCompressor_NSStringVersionNumber;
+
+//! Project version string for LZWCompressor_NSString.
+FOUNDATION_EXPORT const unsigned char LZWCompressor_NSStringVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LZWCompressor_NSString/PublicHeader.h>
+
+#import <LZWCompressor_NSString/LZWEncoder.h>
+#import <LZWCompressor_NSString/LZWDecoder.h>
+#import <LZWCompressor_NSString/LZWTrie.h>
+#import <LZWCompressor_NSString/LZWParser.h>
+#import <LZWCompressor_NSString/LZWWriter.h>
